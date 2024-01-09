46214669fcb38c2c7b7a0bc234e4ef036c9e90c2
MiniAppleTheApple <chenxiuhua19780828@gmail.com>
2024-01-09 13:19:50 -0300

    :pencil: change the apllication mod from 'Elppa.Application' to 'Mark.Application'

--- mix.exs
+++ mix.exs
@@ -15,7 +15,7 @@ defmodule NekoPreto.MixProject do
   def application do
     [
       extra_applications: [:logger],
-      mod: {Elppa.Application, []}
+      mod: {Mark.Application, []}
     ]
   end
 
