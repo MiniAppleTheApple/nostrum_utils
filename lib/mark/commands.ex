94b7b178d925a26ddaa58800d9e27d59dcde8f0c
MiniAppleTheApple <chenxiuhua19780828@gmail.com>
2024-01-21 00:41:36 -0300

    :speech_ballon: Change the commands and options descriptions

--- lib/mark/commands.ex
+++ lib/mark/commands.ex
@@ -19,8 +19,10 @@ defmodule Mark.Commands do
     # Global application commands take a couple of minutes to update in Discord,
     # so we use a test guild when in dev mode.
     if Application.get_env(:mark, :env) == :dev do
+      IO.puts("Dev mode")
       guild_id = Application.get_env(:mark, :dev_guild_id)
-      Nostrum.Api.bulk_overwrite_guild_application_commands(guild_id, commands)
+      {:ok, data} = Nostrum.Api.bulk_overwrite_guild_application_commands(guild_id, commands)
+      IO.inspect(data)
     else
       Nostrum.Api.bulk_overwrite_global_application_commands(commands)
     end
--- lib/mark/commands/mark.ex
+++ lib/mark/commands/mark.ex
@@ -1,6 +1,7 @@
 defmodule Mark.Commands.Mark do
   alias Nostrum.Api
-  
+  alias Nostrum.ApplicationCommandInteractionDataOption
+
   alias Mark.Command
   alias Mark.Constant.ApplicationCommandOptionType
 
@@ -14,19 +15,22 @@ defmodule Mark.Commands.Mark do
       options: [
         %{
           name: "set",
-          description: "The command for setting up the ui",
+          description: "設定UI",
           type: ApplicationCommandOptionType.sub_command(),
         },
         %{
           name: "name",
+          description: "設定取名限制",
           type: ApplicationCommandOptionType.sub_command_group(),
           options: [
             %{
               name: "set",
+              description: "新增新的可以使用的字符格式",
               type: ApplicationCommandOptionType.sub_command(),
               options: [
                 %{
                   name: "type",
+                  description: "字符格式的類型（用','把不同的字符格式代碼分開）",
                   type: ApplicationCommandOptionType.string(),
                 }
               ]
@@ -37,9 +41,17 @@ defmodule Mark.Commands.Mark do
     }
   end
 
+  
+
   @impl Command
   def handle_interaction(interaction) do
-    IO.inspect(interaction) 
+    IO.inspect(interaction)
+    Api.create_interaction_response!(interaction, %{
+      type: 4,
+      data: %{
+        content: "Mark!",
+      }
+    }) 
   end
   
 end
