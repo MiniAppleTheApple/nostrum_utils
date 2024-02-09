15d4b20edcd82a6ce6e40230d2a2278a3159ca26
MiniAppleTheApple <Chenxiuhua19780828@gmail.com>
2024-02-09 14:18:14 -0300

    Let the tests pass

--- lib/mark/message_component/agent.ex
+++ lib/mark/message_component/agent.ex
@@ -5,34 +5,40 @@ defmodule Mark.MessageComponent.Agent do
 
   @spec start_link(map()) :: Agent.on_start()
   def start_link(initial_value) do
+    IO.inspect(initial_value)
     Agent.start_link(fn -> initial_value end, name: __MODULE__)
   end
 
-
   def value do
-    Agent.get(__MODULE__, &(&1))
+    Agent.get(__MODULE__, & &1)
   end
 
   def add_listener(id, listener) do
     Agent.update(__MODULE__, &Map.put(&1, id, listener))
   end
 
-
-
   @spec add_component(MessageComponent.t()) :: nil
   def add_component(%MessageComponent{data: data, handle: handle}) do
     add_listener(data.custom_id, handle)
   end
 
+  @spec trigger(String.t(), Interaction.t()) :: :ok | :error | :remove
   def trigger(id, interaction) do
     Agent.get(__MODULE__, fn x ->
       listener = Map.get(x, id)
+
       if listener do
         listener.(interaction)
-        :ok
       else
         :error
       end
+    end)
+  end
+
+  def remove_component(id) do
+    Agent.update(__MODULE__, fn x ->
+      x
+      |> Map.delete(id)
     end)
   end
 end
