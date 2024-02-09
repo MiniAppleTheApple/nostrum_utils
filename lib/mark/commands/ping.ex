198b6b3e17bd1966f32626edcaa5df9805dd59a4
MiniAppleTheApple <Chenxiuhua19780828@gmail.com>
2024-02-09 18:08:57 -0300

    Remove the button after clicking the button generated by ping command

--- lib/mark/commands/ping.ex
+++ lib/mark/commands/ping.ex
@@ -2,8 +2,11 @@ defmodule Mark.Commands.Ping do
   alias Mark.MessageComponent
 
   alias Nostrum.Api
+
   alias Nostrum.Struct.Component
   alias Nostrum.Struct.Component.ActionRow
+
+  alias Nostrum.Cache.Me
 
   alias Mark.Command
 
@@ -20,19 +23,19 @@ defmodule Mark.Commands.Ping do
   @impl Command
   def handle_interaction(interaction) do
     button = %MessageComponent{
-      data: %Component{
-        type: 2,
-        label: "Dingding",
+      data: Component.button("This button will be deleted after clicked"),
         custom_id: MessageComponent.random_id(),
         style: 1
       },
-      handle: fn interaction ->
-        Api.create_interaction_response!(interaction, %{
+      handle: fn new_interaction ->
+        Api.create_interaction_response!(new_interaction, %{
           type: 4,
           data: %{
             content: "Ping by clicking the button"
           }
         })
+        Api.delete_interaction_response!(Me.get().id, interaction.token)
+        :remove
       end
     }
 
