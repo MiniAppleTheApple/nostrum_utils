e953979c08aeadbfe28adbaeea14d64408856726
MiniAppleTheApple <Chenxiuhua19780828@gmail.com>
2024-05-18 20:16:50 -0300

    feat: implement the command

--- lib/mark/commands/mark/role/delset.ex
+++ lib/mark/commands/mark/role/delset.ex
@@ -1,9 +1,19 @@
 defmodule Mark.Commands.Mark.Role.DelSet do
+  alias Nostrum.Struct.Component.ActionRow
   alias Nostrum.Api
 
-  alias Nostrum.Constants.ApplicationCommandOptionType
+  alias Nostrum.Constants.{ApplicationCommandOptionType, InteractionCallbackType}
+  alias Nostrum.Struct.Component.SelectMenu
+  alias Nostrum.Struct.Component.Option
 
+  alias Mark.Util
+  alias Mark.Listeners
   alias Mark.SubCommand
+  alias Mark.Repo
+  alias Mark.Schema.Role
+
+  import Ecto.Query
+  import Bitwise
 
   @behaviour SubCommand
 
@@ -18,11 +28,55 @@ defmodule Mark.Commands.Mark.Role.DelSet do
 
   @impl SubCommand
   def handle_interaction(interaction, _option) do
-    Api.create_interaction_response!(interaction, %{
-      type: 4,
-      data: %{
-        content: "Ping"
-      }
-    })
+    server = Repo.one!(Util.query_corresponding_server(interaction.guild_id, [:needed_roles]))  
+    case server.needed_roles do
+      [] ->
+        Api.create_interaction_response!(interaction, %{
+          type: InteractionCallbackType.channel_message_with_source(),
+          data: %{
+            flags: 1 <<< 6, #empheral
+            content: "並沒有設定任何身份組",
+          },
+        })  
+      needed_roles ->
+        id = Util.random_id()
+        roles = Api.get_guild_roles!(interaction.guild_id)
+        |> Enum.map(fn role ->
+          {role.id, role.name}
+        end)
+        |> Enum.into(%{})
+        select_menu = SelectMenu.select_menu(id, [
+          min_values: 1,
+          max_values: needed_roles |> Enum.count(),
+          options: needed_roles |> Enum.map(fn role ->
+            %Option{
+              label: roles[role.ref |> String.to_integer()],
+              value: role.ref,
+            }
+          end)
+        ])
+        
+        Listeners.add_listener(id, fn interaction ->
+          values = interaction.data.values
+          query = from r in Role, where: r.ref in ^values, select: r
+          {count, _deleted} = Repo.delete_all(query)
+
+          Api.create_interaction_response!(interaction, %{
+            type: InteractionCallbackType.channel_message_with_source(),
+            data: %{
+              flags: 1 <<< 6, #empheral
+              content: "已在設定中移除#{count}個身份組"
+            }
+          })
+        end)
+
+        Api.create_interaction_response!(interaction, %{
+          type: InteractionCallbackType.channel_message_with_source(),
+          data: %{
+            flags: 1 <<< 6, #empheral
+            components: [ActionRow.action_row(select_menu)]
+          },
+        })
+    end
   end
 end
--- lib/mark/util.ex
+++ lib/mark/util.ex
@@ -1,5 +1,8 @@
 defmodule Mark.Util do
   alias Nostrum.Struct.Interaction
+  alias Mark.Schema.Server
+
+  import Ecto.Query
   @doc """
   Gets an option by it's name from the interaction. Returns `nil` if the
   option is not present in the interaction.
@@ -26,5 +29,10 @@ defmodule Mark.Util do
       |> Map.get(:components)
       |> List.first()
     end)
+  end
+
+  @spec query_corresponding_server(String.t(), [atom()]) :: Queryable.t()
+  def query_corresponding_server(guild_id, preload \\ []) do
+    from s in Server, where: s.ref == ^(guild_id |> to_string()), preload: ^preload, select: s
   end
 end
