3092fa5026da6737c7fc782af61a8ed0f4500fda
MiniAppleTheApple <Chenxiuhua19780828@gmail.com>
2024-05-21 17:32:38 -0300

    remove arguments from the command

--- lib/mark/commands/mark/name/set.ex
+++ lib/mark/commands/mark/name/set.ex
@@ -13,23 +13,12 @@ defmodule Mark.Commands.Mark.Name.Set do
       name: name,
       description: "新增新的可以使用的字符格式",
       type: ApplicationCommandOptionType.sub_command(),
-      options: [
-        %{
-          name: "type",
-          type: ApplicationCommandOptionType.string(),
-          description: "字符格式的類型"
-        }
-      ]
+      options: []
     }
   end
 
   @impl SubCommand
   def handle_interaction(interaction, _option) do
-    Api.create_interaction_response!(interaction, %{
-      type: 4,
-      data: %{
-        content: "Ping"
-      }
-    })
+   
   end
 end
